ce7f4597bc3a8d5284d384db231df39789519ef1
campus2 H015 <h015@alpacoedu.onmicrosoft.com>
2024-12-11 18:28:19 +0900

    new project

---
+++ nologal/src/main/java/com/hs917ouo/nologal/NologalApplication.java
@@ -0,0 +1,13 @@
+package com.hs917ouo.nologal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NologalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NologalApplication.class, args);
+	}
+
+}
---
+++ nologal/src/test/java/com/hs917ouo/nologal/NologalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs917ouo.nologal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NologalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
